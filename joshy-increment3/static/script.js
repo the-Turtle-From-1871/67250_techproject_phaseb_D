d2ff05b426bcc50a485a1d756c3fcb7b14ef6d6e
the-Turtle-From-1871 <103733372+the-Turtle-From-1871@users.noreply.github.com>
2026-03-16 16:03:25 -0400

    Update script.js

--- joshy-increment3/static/script.js
+++ joshy-increment3/static/script.js
@@ -1,9 +1,16 @@
 var x = 6;
 var y = 7;
 var z = x + y;
-console.log(z);
+// console.log(z);
 
 var a = "Josh";
 var b = "Yang";
 var c = a + b;
-console.log(c);
+// console.log(c);
+
+function sumnPrint(x1, x2) {
+    console.log(x1 + x2);
+}
+
+SumnPrint(x, y);
+SumnPrint(a, b);
