ee4f51ef640a3eca6d4a8ce0aaa0566bd8bf2beb
the-Turtle-From-1871 <103733372+the-Turtle-From-1871@users.noreply.github.com>
2026-03-23 18:50:39 -0400

    update

---
+++ joshy-increment4/static/script.js
@@ -0,0 +1,105 @@
+var x = 5;
+var y = 7;
+var z = x + y;
+// console.log(z);
+
+var a = "Hello";
+var b = " World!";
+var c = a + b;
+// console.log(c);
+
+function sumnPrint(x1, x2) {
+    console.log(x1 + x2);
+}
+
+// sumnPrint(x, y);
+// sumnPrint(a, b);
+// console.log(c.length);
+
+
+var date = new Date();
+var year = date.getFullYear();
+document.getElementById("copyright").innerHTML = "&copy " + year + " Monomuse Inc. All rights reserved.";
+
+
+// function addYear() {
+//     var date = new Date();
+//     var year = date.getFullYear();
+//     document.getElementById("copyYear").innerHTML = year;
+// }
+
+
+if (c.length > z) {
+    console.log(c);
+} else if (c.length < z) {
+        console.log(z);
+    } else {
+    console.log("good job!");
+}
+
+L1 = ["Watermelon","Pineapple","Pear","Banana"];
+L2 = ["Apple","Banana","Kiwi","Orange"];
+
+// version 1
+function findTheBanana(array) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] === "Banana") {
+            alert("Found Banana!");
+        }
+    }
+}
+
+// findTheBanana(L1);
+// findTheBanana(L2);
+
+
+// version 2
+function findTheBanana2(array) {
+    array.forEach((item) => {
+        if (item === "Banana") {
+            alert("Found it");
+        }
+    });
+}
+
+
+
+// findTheBanana2(L1);
+// findTheBanana2(L2);
+
+// date/time based greeting
+var now = new Date();
+var hour = now.getHours();
+
+function greeting(hour) {
+    if (document.getElementById("greeting") === null) {
+        return;
+    }  
+    
+    // console.log(hour);
+    var welcome = "and welcome to MonoMuse."
+    if (hour < 5 | hour >= 20) {
+        document.getElementById("greeting").innerHTML = "Good night" ;
+    } else if (hour < 12) {
+        document.getElementById("greeting").innerHTML = "Good morning, " + welcome;
+    } else if (hour < 18) {
+        document.getElementById("greeting").innerHTML = "Good afternoon, " + welcome;
+    } else {
+        document.getElementById("greeting").innerHTML = "Good evening, " + welcome;
+    }
+}
+
+greeting(hour);
+
+// active nav bar (new implementation)
+function ActiveNav() {
+    var navLinks = document.querySelectorAll('nav li a');
+    console.log(navLinks);
+    navLinks.forEach(element => {
+        var activePage = window.location.href;
+        if (activePage == element.href) {
+            element.classList.add("active");
+        }
+    });
+}
+ActiveNav();
