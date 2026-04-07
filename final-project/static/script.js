0f21be4719c4cccf55ac751e330c227252bb841d
the-Turtle-From-1871 <103733372+the-Turtle-From-1871@users.noreply.github.com>
2026-04-06 22:11:33 -0400

    updated attributions

--- final-project/static/script.js
+++ final-project/static/script.js
@@ -1,71 +1,6 @@
-var x = 5;
-var y = 7;
-var z = x + y;
-// console.log(z);
-
-var a = "Hello";
-var b = " World!";
-var c = a + b;
-// console.log(c);
-
-function sumnPrint(x1, x2) {
-    console.log(x1 + x2);
-}
-
-// sumnPrint(x, y);
-// sumnPrint(a, b);
-// console.log(c.length);
-
-
 var date = new Date();
 var year = date.getFullYear();
 document.getElementById("copyright").innerHTML = "&copy " + year + " Monomuse Inc. All rights reserved.";
-
-
-// function addYear() {
-//     var date = new Date();
-//     var year = date.getFullYear();
-//     document.getElementById("copyYear").innerHTML = year;
-// }
-
-
-if (c.length > z) {
-    console.log(c);
-} else if (c.length < z) {
-        console.log(z);
-    } else {
-    console.log("good job!");
-}
-
-L1 = ["Watermelon","Pineapple","Pear","Banana"];
-L2 = ["Apple","Banana","Kiwi","Orange"];
-
-// version 1
-function findTheBanana(array) {
-    for (var i = 0; i < array.length; i++) {
-        if (array[i] === "Banana") {
-            alert("Found Banana!");
-        }
-    }
-}
-
-// findTheBanana(L1);
-// findTheBanana(L2);
-
-
-// version 2
-function findTheBanana2(array) {
-    array.forEach((item) => {
-        if (item === "Banana") {
-            alert("Found it");
-        }
-    });
-}
-
-
-
-// findTheBanana2(L1);
-// findTheBanana2(L2);
 
 // date/time based greeting
 var now = new Date();
@@ -124,7 +59,7 @@ ActiveNav();
   });
 
 // calendar logic for checkout page
-// https://webdesign.tutsplus.com/learn-how-to-code-a-simple-javascript-calendar-and-datepicker--cms-108322t
+// inspiration & source: https://webdesign.tutsplus.com/learn-how-to-code-a-simple-javascript-calendar-and-datepicker--cms-108322t
 
 // current month and year
 let currentMonth = new Date().getMonth(); 
